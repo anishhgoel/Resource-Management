db16530215281ffa2537a330143ca67c94c7dbf6
xcodesgit <anishgoel@berkeley.edu>
2025-02-01 15:38:18 -0800

    added middleware for authentication

---
+++ backend/middleware/auth.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+const jwtSecret = process.env.JWT_SECRET || 'secretkey';
+
+export default function auth(req, res, next) {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+}
